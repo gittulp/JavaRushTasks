5eebed2c1ca59fb75e1221c2aad59b8eb70bc866
gittulp <raufovi4@yandex.ru>
2020-05-17 03:24:50 +0500

    JavaCore/task13/task1325 done

--- 2.JavaCore/src/com/javarush/task/task13/task1325/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1325/Solution.java
@@ -4,6 +4,8 @@ package com.javarush.task.task13.task1325;
 Компиляция программы
 */
 
+import java.awt.*;
+
 public class Solution {
 
     public static void main(String[] args) throws Exception {
@@ -16,14 +18,18 @@ public class Solution {
         Color getColor();
     }
 
-    public static class Fox implements Animal {
+    public static abstract class Fox implements Animal {
         public String getName() {
             return "Fox";
         }
     }
 
-    public abstract static class BigFox {
+    public static class BigFox extends Fox{
 
+        @Override
+        public Color getColor() {
+            return Color.BLACK;
+        }
     }
 
 }
